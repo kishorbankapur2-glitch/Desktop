b1859f8a4d964bfcd3904fff27916a0d4b49ec56
kishorbankapur2-glitch <kishorbankapur2@gmail.com>
2026-04-01 16:09:58 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "win11-launcher-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./desktop.png"
+];
+
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache => {
+
+return cache.addAll(urlsToCache);
+
+})
+
+);
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response => {
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});

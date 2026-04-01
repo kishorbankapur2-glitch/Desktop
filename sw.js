1a4094ee2042959da6614bee9eaf15209561122e
kishorbankapur2-glitch <kishorbankapur2@gmail.com>
2026-04-01 16:44:36 +0530

    Update service worker cache name and structure

--- sw.js
+++ sw.js
@@ -1,36 +1,39 @@
-const CACHE_NAME = "win11-v2";
+const CACHE_NAME = "win11-launcher-v1";
 
 const urlsToCache = [
-  "/Desktop/",
-  "/Desktop/index.html",
-  "/Desktop/manifest.json",
-  "/Desktop/desktop.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./desktop.png"
 ];
 
+// Install
 self.addEventListener("install", event => {
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-
-.then(cache => cache.addAll(urlsToCache))
-
-);
-
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
-
-event.respondWith(
-
-caches.match(event.request)
-
-.then(response => {
-
-return response || fetch(event.request);
-
-})
-
-);
-
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
